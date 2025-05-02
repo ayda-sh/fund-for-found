2f3642abaff58949c0c745a552a63175eb047af3
ayda <aydashamma4@gmail.com>
2025-05-02 16:22:33 +0330

    feat: added loading for forms

--- src/app/components/login/loginform.jsx
+++ src/app/components/login/loginform.jsx
@@ -12,6 +12,7 @@ import axios from "axios";
 function LoginForm() {
   const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
   const schema = yup.object({
@@ -37,6 +38,7 @@ function LoginForm() {
   });
 
   const submitLoginForm = async (user) => {
+    setIsLoading(true);
     try {
       const response = await axios.post(
         "https://fund-for-found-backend-13.onrender.com/api/auth/local",
@@ -46,7 +48,9 @@ function LoginForm() {
       localStorage.setItem("token", response.data.jwt);
       router.push("/");
     } catch (error) {
-        setError(error);
+      setError(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -184,7 +188,32 @@ function LoginForm() {
                 type="submit"
                 className="w-full bg-[var(--primary)] text-white py-3.5 rounded-md focus:outline-none mt-6"
               >
-                Continue
+                {isLoading ? (
+                   <span className="flex justify-center items-center gap-2">
+                  <svg
+                    className="animate-spin h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                  </span>
+                ) : (
+                  "Continue"
+                )}
               </button>
             </form>
             <div className="flex flex-col justify-center text-center gap-2">
