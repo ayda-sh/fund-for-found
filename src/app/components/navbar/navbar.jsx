0d6b2fa291ca19a716dba20c27c58b7350cae0b4
ayda <aydashamma4@gmail.com>
2025-05-04 19:12:21 +0330

    improve: mobile menu

--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -33,7 +33,7 @@ export function NavBar() {
 
   return (
     <header>
-      <nav className="p-4 border-b border-[var(--primary-100)]">
+      <nav className="p-4 border-b border-[var(--primary-100)] w-full bg-white">
         <div className="flex max-w-[1024px] items-center mx-auto justify-between">
           <div className="">
             <Link href="/">
@@ -55,7 +55,10 @@ export function NavBar() {
 
           <div className="items-center justify-center gap-2 hidden md:flex">
             <Search />
-            <a href="/sign-up" className="bg-[var(--primary)] py-3 px-7 rounded-xl text-sm text-white">
+            <a
+              href="/sign-up"
+              className="bg-[var(--primary)] py-3 px-7 rounded-xl text-sm text-white"
+            >
               Sign in
             </a>
           </div>
@@ -77,24 +80,39 @@ export function NavBar() {
                   />
                 </svg>
               </button>
-              <button
-                className="md:hidden focus:outline-none focus:ring focus:ring-purple-500"
-                onClick={toggleMenu}
-              >
-                <svg
-                  className="size-9 text-[var(--primary)]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
+              <button className="md:hidden focus:outline-none focus:ring focus:ring-purple-500" onClick={toggleMenu}>
+              
+                {isOpen ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-9 text-[var(--primary)]"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18 18 6M6 6l12 12"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="size-9 text-[var(--primary)]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                    />
+                  </svg>
+                )}
               </button>
             </div>
           </div>
@@ -106,36 +124,56 @@ export function NavBar() {
           </div>
         )}
 
-        {isOpen && (
-          <ul className="md:hidden flex flex-col space-y-2 mt-2">
+        
+      </nav>
+      {isOpen && (
+          <div className="absolute flex flex-col justify-between z-40 h-[600px] bg-white w-full border-l border-r border-b border-[var(--light-3)] border-t-0 rounded-bl-2xl rounded-br-2xl">
+          <div className="p-6">
+          <ul className="md:hidden flex flex-col space-y-3.5 mt-2">
             <li>
-              <a href="/" className="block text-gray-800 hover:text-[var(--primary)]">
+              <a
+                href="/"
+                className="block text-[var(--primary)] text-[18px]"
+              >
                 Home
               </a>
             </li>
             <li>
-              <a href="/explore" className="block text-gray-800 hover:text-[var(--primary)]">
+              <a
+                href="/explore"
+                className="block text-[var(--primary)] text-[18px]"
+              >
                 Explore
               </a>
             </li>
             <li>
-              <a href="#" className="block text-gray-800 hover:text-[var(--primary)]">
+              <a
+                href="#"
+                className="block text-[var(--primary)] text-[18px]"
+              >
                 About us
               </a>
             </li>
             <li>
-              <a href="#" className="block text-gray-800 hover:text-[var(--primary)]">
+              <a
+                href="#"
+                className="block text-[var(--primary)] text-[18px]"
+              >
                 Help & Support
               </a>
             </li>
-            <li>
-              <a href="/sign-up" className="block text-gray-800 hover:text-[var(--primary)]">
-                Sign in
-              </a>
-            </li>
+            
           </ul>
+          </div>
+
+          <div className="flex flex-col gap-3 border-t border-[var(--light-3)]">
+           
+           <button className="mt-9 bg-[var(--primary-75)] mx-6 text-[var(--primary)] py-2 rounded border border-[var(--primary-200)]"><Link href="/login">login/signup</Link></button>
+           <button className="mb-9 bg-[var(--primary)] mx-6 text-white py-2 rounded"><Link href="/brand-and-organizations">start</Link></button>
+           
+          </div>
+          </div>
         )}
-      </nav>
     </header>
   );
 }
